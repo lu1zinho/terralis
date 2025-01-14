2ac4ab243f2ffe4e1922427e13dd8ab8c447d1d6
Luiz Antonio Soares Filho <luizantonio.filho@dataprev.gov.br>
2025-01-14 17:30:11 +0200

    Vercel environment variables test

--- lib/http/google_credentials.dart
+++ lib/http/google_credentials.dart
@@ -1,19 +1,32 @@
+import 'dart:io';
 
 String getGoogleCredentials() {
+
+  const type = String.fromEnvironment('TYPE');
+  const projectId = String.fromEnvironment('PROJECT_ID');
+  const privateKeyId = String.fromEnvironment('PRIVATE_KEY_ID');
+  const privateKey = String.fromEnvironment('PRIVATE_KEY');
+  const clientEmail = String.fromEnvironment('CLIENT_EMAIL');
+  const clientId = String.fromEnvironment('CLIENT_ID');
+  const authUri = String.fromEnvironment('AUTH_URI');
+  const tokenUri = String.fromEnvironment('TOKEN_URI');
+  const authProviderX509CertUrl = String.fromEnvironment('AUTH_PROVIDER_X509_CERT_URL');
+  const clientX509CertUrl = String.fromEnvironment('CLIENT_X509_CERT_URL');
+  const universeDomain = String.fromEnvironment('UNIVERSE_DOMAIN');
   
   String googleCredentials = '''
 {
-  "type": "${const String.fromEnvironment('TYPE')}",
-  "project_id": "${const String.fromEnvironment('PROJECT_ID')}",
-  "private_key_id": "${const String.fromEnvironment('PRIVATE_KEY_ID')}",
-  "private_key": "${const String.fromEnvironment('PRIVATE_KEY')}",
-  "client_email": "${const String.fromEnvironment('CLIENT_EMAIL')}",
-  "client_id": "${const String.fromEnvironment('CLIENT_ID')}",
-  "auth_uri": "${const String.fromEnvironment('AUTH_URI')}",
-  "token_uri": "${const String.fromEnvironment('TOKEN_URI')}",
-  "auth_provider_x509_cert_url": "${const String.fromEnvironment('AUTH_PROVIDER_X509_CERT_URL')}",
-  "client_x509_cert_url": "${const String.fromEnvironment('CLIENT_X509_CERT_URL')}",
-  "universe_domain": "${const String.fromEnvironment('UNIVERSE_DOMAIN')}"
+  "type": "${type != '' ? type : Platform.environment['TYPE']}",
+  "project_id": "${projectId != '' ? projectId : Platform.environment['PROJECT_ID']}",
+  "private_key_id": "${privateKeyId != '' ? privateKeyId : Platform.environment['PRIVATE_KEY_ID']}",
+  "private_key": "${privateKey != '' ? privateKey : Platform.environment['PRIVATE_KEY']}",
+  "client_email": "${clientEmail != '' ? clientEmail : Platform.environment['CLIENT_EMAIL']}",
+  "client_id": "${clientId != '' ? clientId : Platform.environment['CLIENT_ID']}",
+  "auth_uri": "${authUri != '' ? authUri : Platform.environment['AUTH_URI']}",
+  "token_uri": "${tokenUri != '' ? tokenUri : Platform.environment['TOKEN_URI']}",
+  "auth_provider_x509_cert_url": "${authProviderX509CertUrl != '' ? authProviderX509CertUrl : Platform.environment['AUTH_PROVIDER_X509_CERT_URL']}",
+  "client_x509_cert_url": "${clientX509CertUrl != '' ? clientX509CertUrl : Platform.environment['CLIENT_X509_CERT_URL']}",
+  "universe_domain": "${universeDomain != '' ? universeDomain : Platform.environment['UNIVERSE_DOMAIN']}"
 }
 ''';
 
