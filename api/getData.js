114bddbe71730ec8b3155aaae704e7cc9ed093a9
Luiz Antonio Soares Filho <luizantonio.filho@dataprev.gov.br>
2025-01-14 18:49:06 +0200

    Create serverless function in Vercel for secure environment variable handling

---
+++ api/getData.js
@@ -0,0 +1,18 @@
+
+module.exports = (req, res) => {
+  const data = {
+    type: process.env.TYPE,
+    projectId: process.env.PROJECT_ID,
+    privateKeyId: process.env.PRIVATE_KEY_ID,
+    privateKey: process.env.PRIVATE_KEY,
+    clientEmail: process.env.CLIENT_EMAIL,
+    clientId: process.env.CLIENT_ID,
+    authUri: process.env.AUTH_URI,
+    tokenUri: process.env.TOKEN_URI,
+    authProviderX509CertUrl: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    clientX509CertUrl: process.env.CLIENT_X509_CERT_URL,
+    universeDomain: process.env.UNIVERSE_DOMAIN,
+  };
+
+  res.status(200).json(data);
+};
