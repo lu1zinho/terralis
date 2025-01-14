378491c2469d92b6542cc8ade8c1dfc15e852fe0
Luiz Antonio Soares Filho <luizantonio.filho@dataprev.gov.br>
2025-01-14 20:28:07 +0200

    Most secure approach to protect the env variables

--- lib/http/google_credentials.dart
+++ lib/http/google_credentials.dart
@@ -1,3 +1,4 @@
+import 'package:universal_html/js.dart' as js;
 
 String getGoogleCredentials() {
 
@@ -15,17 +16,17 @@ String getGoogleCredentials() {
   
   String googleCredentials = '''
 {
-  "type": "$type",
-  "project_id": "$projectId",
-  "private_key_id": "$privateKeyId",
-  "private_key": "$privateKey",
-  "client_email": "$clientEmail",
-  "client_id": "$clientId",
-  "auth_uri": "$authUri",
-  "token_uri": "$tokenUri",
-  "auth_provider_x509_cert_url": "$authProviderX509CertUrl",
-  "client_x509_cert_url": "$clientX509CertUrl",
-  "universe_domain": "$universeDomain"
+  "type": "${type != '' ? type : js.context['TYPE']}",
+  "project_id": "${projectId != '' ? projectId : js.context['PROJECT_ID']}",
+  "private_key_id": "${privateKeyId != '' ? privateKeyId : js.context['PRIVATE_KEY_ID']}",
+  "private_key": "${privateKey != '' ? privateKey : js.context['PRIVATE_KEY']}",
+  "client_email": "${clientEmail != '' ? clientEmail : js.context['CLIENT_EMAIL']}",
+  "client_id": "${clientId != '' ? clientId : js.context['CLIENT_ID']}",
+  "auth_uri": "${authUri != '' ? authUri : js.context['AUTH_URI']}",
+  "token_uri": "${tokenUri != '' ? tokenUri : js.context['TOKEN_URI']}",
+  "auth_provider_x509_cert_url": "${authProviderX509CertUrl != '' ? authProviderX509CertUrl : js.context['AUTH_PROVIDER_X509_CERT_URL']}",
+  "client_x509_cert_url": "${clientX509CertUrl != '' ? clientX509CertUrl : js.context['CLIENT_X509_CERT_URL']}",
+  "universe_domain": "${universeDomain != '' ? universeDomain : js.context['UNIVERSE_DOMAIN']}"
 }
 ''';
 
